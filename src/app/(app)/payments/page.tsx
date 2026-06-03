630a0dcf154656b7567d6f50c88cb8ed9ff6074c
farahzahirdev <farahzahir.dev@gmail.com>
2026-06-03 19:53:28 +0500

    updated checkbox color and text

--- src/app/(app)/payments/page.tsx
+++ src/app/(app)/payments/page.tsx
@@ -281,7 +281,7 @@ export default function PaymentsPage() {
                   type="checkbox"
                   checked={activeAccessOnly}
                   onChange={(e) => setActiveAccessOnly(e.target.checked)}
-                  className="h-4 w-4 rounded border-[#b7c7e6]"
+                  className="h-4 w-4 rounded border-[#b7c7e6] bg-white"
                 />
                 {t('filters.activeOnly')}
               </label>
